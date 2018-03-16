8f07ac296dd6136aaa4246fdb2904492d2be3f03
Zach Auclair <zach101@gmail.com>
2018-03-15 22:05:56 -0400

    feat(rmb-to-usd): implement json interface with backing api

--- rmb-to-usd/handler.py
+++ rmb-to-usd/handler.py
@@ -1,7 +1,52 @@
-def handle(req):
-    """handle a request to the function
+import requests
+import json
+
+endpoint = 'https://free.currencyconverterapi.com/api/v5/convert?q=CNY_USD,USD_CNY&compact=y'
+
+def send_error(key, message):
+    print(json.dumps({"id": key, "message": message}))
+
+def handle(r):
+    """handle a request for conversion, hitting a free converter and returning the response.
     Args:
         req (str): request body
     """
+    try:
+        if r == "":
+            req = {}
+        else:
+            req = json.loads(r)
+    except json.decoder.JSONDecodeError:
+        return send_error("unprocessable_entity", "the request body was malformed")
 
-    print("sup" + req)
+    to = req.get("to")
+    if to not in ["RMB", "USD", "CNY"]:
+        return send_error("unprocessable_entity", "invalid parameter to, must be RMB, CNY, or USD")
+    if to == "RMB":
+        to = "CNY"
+
+    if to == "CNY":
+        fro = "USD"
+    else:
+        fro = "CNY"
+
+    count = req.get("ammount", req.get("count", 1))
+    try:
+        count = int(count)
+        if count < 1:
+            raise ValueError()
+    except ValueError:
+        return send_error("unprocessable_entity", "invalid parameter for ammount/count, must be positive integer")
+
+    key = "%s_%s" % (fro, to)
+    try:
+        rate = requests.get(endpoint, timeout=5).json()[key]["val"]
+    except requests.exceptions.Timeout:
+        return send_error("service_unavailable", "timeout accessing service")
+    except:
+        return send_error("internal_error", "an error occurred")
+
+    print(json.dumps({"rate": rate, "from_to": key, "value": rate * count}))
+
+#if __name__ == "__main__":
+#    handle('{"to":"USD", "count": "10"}')
